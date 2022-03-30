e2598c12929e9c956bdb2f3c674120eee29758f0
Aktarus006 <joachim.bastin@diagenode.com>
2022-03-30 11:46:20 +0200

    feature: new MenuLink component with hover underline effect

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+html,
+body,
+#app {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: linear-gradient(darkcyan, black);
+}
+</style>
--- apps/front/pages/index.vue
+++ apps/front/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    Hello
+    <p class="hover-underline-animation">
+      <akt-MenuLink path-url="/about">
+        Hey man
+      </akt-MenuLink>
+    </p>
   </div>
   <akt-DarkToggle />
 </template>
