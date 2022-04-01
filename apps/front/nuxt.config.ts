bb21b91c6cabdd788004c412f4034170014c99ef
Aktarus006 <joachim.bastin@diagenode.com>
2022-04-01 11:56:52 +0200

    feat: add nuxt strapi

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     '@aktarus006/ui/nuxt',
     '@vueuse/nuxt',
     '@unocss/nuxt',
+    '@nuxtjs/strapi',
   ],
 
   vueuse: {
@@ -22,5 +23,10 @@ export default defineNuxtConfig({
     shortcuts: [
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
+  },
+  strapi: {
+    url: 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
   },
 })
