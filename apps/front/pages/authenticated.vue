8e6b946e09271b5dacefd02952e5d054f2762535
Aktarus006 <joachim.bastin@diagenode.com>
2022-04-01 11:56:41 +0200

    feat: authenticated page

---
+++ apps/front/pages/authenticated.vue
@@ -0,0 +1,5 @@
+<template>
+  <div w-full h-full>
+    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="">
+  </div>
+</template>
